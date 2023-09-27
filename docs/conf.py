7e484f5066505e7a2b95c005aeb4577f38f55c60
Gerrishon <scalabli@pm.me>
2023-09-27 15:06:39 -0400

    added new changes

--- docs/conf.py
+++ docs/conf.py
@@ -130,7 +130,7 @@ html_theme_options = {'collapse_navigation': False, 'logo_only': True}
 
 # The name of an image file (relative to this directory) to place at the top
 # of the sidebar.
-html_logo = "images/quo.png"
+html_logo = "images/peritia.png"
 
 # The name of an image file (within the static path) to use as favicon of the
 # docs.  This file should be a Windows icon file (.ico) being 16x16 or 32x32
